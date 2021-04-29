cf6d442eb42178f6493de1eda1c96951575bedc2
Lars Boegild Thomsen <lth@netcompartner.com>
2021-04-29 23:31:01 +0800

    USB Working

--- compusb/Core/Src/main.c
+++ compusb/Core/Src/main.c
@@ -93,7 +93,6 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  //MX_USB_OTG_FS_PCD_Init();
   /* USER CODE BEGIN 2 */
   MX_USB_DEVICE_Init();
   /* USER CODE END 2 */
@@ -184,7 +183,7 @@ static void MX_USB_OTG_FS_PCD_Init(void)
   hpcd_USB_OTG_FS.Init.speed = PCD_SPEED_FULL;
   hpcd_USB_OTG_FS.Init.dma_enable = DISABLE;
   hpcd_USB_OTG_FS.Init.phy_itface = PCD_PHY_EMBEDDED;
-  hpcd_USB_OTG_FS.Init.Sof_enable = ENABLE;
+  hpcd_USB_OTG_FS.Init.Sof_enable = DISABLE;
   hpcd_USB_OTG_FS.Init.low_power_enable = DISABLE;
   hpcd_USB_OTG_FS.Init.lpm_enable = DISABLE;
   hpcd_USB_OTG_FS.Init.vbus_sensing_enable = DISABLE;
--- compusb/usb/Target/usbd_conf.c
+++ compusb/usb/Target/usbd_conf.c
@@ -67,63 +67,63 @@ USBD_StatusTypeDef USBD_Get_USB_Status(HAL_StatusTypeDef hal_status);
 *******************************************************************************/
 /* MSP Init */
 
-void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(pcdHandle->Instance==USB_OTG_FS)
-  {
-  /* USER CODE BEGIN USB_OTG_FS_MspInit 0 */
-
-  /* USER CODE END USB_OTG_FS_MspInit 0 */
-
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**USB_OTG_FS GPIO Configuration
-    PA11     ------> USB_OTG_FS_DM
-    PA12     ------> USB_OTG_FS_DP
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_11|GPIO_PIN_12;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-    /* Peripheral clock enable */
-    __HAL_RCC_USB_OTG_FS_CLK_ENABLE();
-
-    /* Peripheral interrupt init */
-    HAL_NVIC_SetPriority(OTG_FS_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(OTG_FS_IRQn);
-  /* USER CODE BEGIN USB_OTG_FS_MspInit 1 */
-
-  /* USER CODE END USB_OTG_FS_MspInit 1 */
-  }
-}
-
-void HAL_PCD_MspDeInit(PCD_HandleTypeDef* pcdHandle)
-{
-  if(pcdHandle->Instance==USB_OTG_FS)
-  {
-  /* USER CODE BEGIN USB_OTG_FS_MspDeInit 0 */
-
-  /* USER CODE END USB_OTG_FS_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_USB_OTG_FS_CLK_DISABLE();
-
-    /**USB_OTG_FS GPIO Configuration
-    PA11     ------> USB_OTG_FS_DM
-    PA12     ------> USB_OTG_FS_DP
-    */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
-
-    /* Peripheral interrupt Deinit*/
-    HAL_NVIC_DisableIRQ(OTG_FS_IRQn);
-
-  /* USER CODE BEGIN USB_OTG_FS_MspDeInit 1 */
-
-  /* USER CODE END USB_OTG_FS_MspDeInit 1 */
-  }
-}
+//void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle)
+//{
+//  GPIO_InitTypeDef GPIO_InitStruct = {0};
+//  if(pcdHandle->Instance==USB_OTG_FS)
+//  {
+//  /* USER CODE BEGIN USB_OTG_FS_MspInit 0 */
+//
+//  /* USER CODE END USB_OTG_FS_MspInit 0 */
+//
+//    __HAL_RCC_GPIOA_CLK_ENABLE();
+//    /**USB_OTG_FS GPIO Configuration
+//    PA11     ------> USB_OTG_FS_DM
+//    PA12     ------> USB_OTG_FS_DP
+//    */
+//    GPIO_InitStruct.Pin = GPIO_PIN_11|GPIO_PIN_12;
+//    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+//    GPIO_InitStruct.Pull = GPIO_NOPULL;
+//    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+//    GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
+//    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+//
+//    /* Peripheral clock enable */
+//    __HAL_RCC_USB_OTG_FS_CLK_ENABLE();
+//
+//    /* Peripheral interrupt init */
+//    HAL_NVIC_SetPriority(OTG_FS_IRQn, 0, 0);
+//    HAL_NVIC_EnableIRQ(OTG_FS_IRQn);
+//  /* USER CODE BEGIN USB_OTG_FS_MspInit 1 */
+//
+//  /* USER CODE END USB_OTG_FS_MspInit 1 */
+//  }
+//}
+//
+//void HAL_PCD_MspDeInit(PCD_HandleTypeDef* pcdHandle)
+//{
+//  if(pcdHandle->Instance==USB_OTG_FS)
+//  {
+//  /* USER CODE BEGIN USB_OTG_FS_MspDeInit 0 */
+//
+//  /* USER CODE END USB_OTG_FS_MspDeInit 0 */
+//    /* Peripheral clock disable */
+//    __HAL_RCC_USB_OTG_FS_CLK_DISABLE();
+//
+//    /**USB_OTG_FS GPIO Configuration
+//    PA11     ------> USB_OTG_FS_DM
+//    PA12     ------> USB_OTG_FS_DP
+//    */
+//    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
+//
+//    /* Peripheral interrupt Deinit*/
+//    HAL_NVIC_DisableIRQ(OTG_FS_IRQn);
+//
+//  /* USER CODE BEGIN USB_OTG_FS_MspDeInit 1 */
+//
+//  /* USER CODE END USB_OTG_FS_MspDeInit 1 */
+//  }
+//}
 
 /**
   * @brief  Setup stage callback
