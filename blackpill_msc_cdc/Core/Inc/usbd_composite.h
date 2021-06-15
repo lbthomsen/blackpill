b282596fac99f5158b0274d30c86e479367b71e5
Lars Boegild Thomsen <lth@netcompartner.com>
2021-06-15 10:18:29 +0800

    Hmmmm

---
+++ blackpill_msc_cdc/Core/Inc/usbd_composite.h
@@ -0,0 +1,43 @@
+#ifndef __USBD_COMPOSITE_H
+#define __USBD_COMPOSITE_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include  "usbd_ioreq.h"
+#include "usbd_cdc.h"
+#include "usbd_msc.h"
+
+#define MC_MAX_FS_PACKET            0x40
+
+#define USB_MC_CONFIG_DESC_SIZ      106
+
+#define MC_MSC_EPIN_ADDR                MSC_EPIN_ADDR
+#define MC_MSC_EPOUT_ADDR               MSC_EPOUT_ADDR
+
+#define MC_CDC_IN_EP                   CDC_IN_EP
+#define MC_CDC_OUT_EP                  CDC_OUT_EP
+#define MC_CDC_CMD_EP                  CDC_CMD_EP
+
+extern USBD_ClassTypeDef  USBD_MC;
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* __USBD_MSC_H */
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT WEYNE *****END OF FILE****/
