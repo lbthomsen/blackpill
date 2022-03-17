a156686e3783bc817bd9298c04d458cacf8033bc
Lars Boegild Thomsen <lth@netcompartner.com>
2022-03-17 16:43:07 +0800

    Moving to stm32cubeide 1.9.0

--- eeprom/Core/Src/main.c
+++ eeprom/Core/Src/main.c
@@ -47,7 +47,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-SPI_HandleTypeDef hspi1;
+ SPI_HandleTypeDef hspi1;
 
 UART_HandleTypeDef huart1;
 
@@ -233,6 +233,7 @@ void SystemClock_Config(void)
   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
@@ -248,6 +249,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+
   /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
@@ -411,4 +413,3 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
--- eeprom/Core/Src/stm32f4xx_it.c
+++ eeprom/Core/Src/stm32f4xx_it.c
@@ -216,4 +216,3 @@ void SPI1_IRQHandler(void)
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
-
