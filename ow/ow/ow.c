81c09b559994b4b77dd6b3cfdd6dc6a2aae82926
Lars Boegild Thomsen <lth@netcompartner.com>
2023-09-08 17:39:15 +0800

    Hmmmm

---
+++ ow/ow/ow.c
@@ -0,0 +1,22 @@
+/**
+ ******************************************************************************
+ * @file           : ow.c
+ * @brief          : ow library
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 Lars Boegild Thomsen <lbthomsen@gmail.com>
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include "main.h"
+
+#include "ow.h"
+
+
---
+++ ow/ow/ow.h
@@ -0,0 +1,23 @@
+/**
+ ******************************************************************************
+ * @file           : ow.h
+ * @brief          : ow library header
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 Lars Boegild Thomsen <lbthomsen@gmail.com>
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#ifndef OW_H_
+#define OW_H_
+
+
+
+#endif /* OW_H_ */
